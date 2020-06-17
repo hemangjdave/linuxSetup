0d12e587494f19d038328e599ea066cc9e0bcc3f
hemangjdave <hemangjdave@gmail.com>
2020-06-18 00:41:08 +0530

    Adding shell script to setup basic softwares

---
+++ setupLinuxDeb.sh
@@ -0,0 +1,9 @@
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt-get install vim-gtk3 bleachbit gdebi terminator git tlp gufw gnome-tweaks chromium-browser -y
+
+sudo add-apt-repository ppa:oguzhaninan/stacer -y
+sudo apt-get update
+sudo apt-get install stacer -y
+
+sudo snap install rambox
